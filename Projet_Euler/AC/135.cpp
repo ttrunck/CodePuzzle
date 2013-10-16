b761e655d506993d7e2c8f8816caf3f98d21b5a0
Blabla404 <blabla404@free.fr>
2013-10-16 20:30:54 +0200

    135 136 euler

---
+++ Projet_Euler/AC/135.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+const int N=1000000;
+const int sol=10; 
+
+typedef long long ll;
+
+int main(){
+  vector<int> compt(N);
+
+  for(ll u=1;u<N;++u)
+    for(ll v=(u+2)/3+1;v*u<N;++v){
+      if((u+v)%4==0 && (3*v-u)%4==0){
+	//cerr << u << ' ' << v << endl;
+	compt[u*v]++;
+      }
+    }
+  
+  int res=0;
+  for(int x:compt)
+    if(x==sol)
+      res++;
+  
+  cout << res << endl;
+}
